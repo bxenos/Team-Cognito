577ff63d53283ac847459ada11e63b1c1c8734ae
Gabriel Tinsley <gabrieltinsley@u.boisestate.edu>
2025-04-25 22:06:01 -0600

    Add button works on website

--- src/server.js
+++ src/server.js
@@ -1,25 +1,34 @@
 import express from 'express'
 import sql from 'sqlite3'
+import path from 'path'
 
 const sqlite3 = sql.verbose()
 
-// Create an in memory table to use
 const db = new sqlite3.Database(':memory:')
 
 const app = express()
+
 app.use(express.static('public'))
-app.set('views', 'views')
-app.set('view engine', 'pug')
 app.use(express.urlencoded({ extended: false }))
+app.set('views', 'views') // <- Set views folder
+app.set('view engine', 'pug') // <- Use pug engine
 
+// GET request for the main form
 app.get('/', function (req, res) {
-  console.log('GET called')
-  res.render('index')
+    console.log('GET / called')
+    res.render('index')
 })
 
-// add more webpages
+// POST request to create the user page
+app.post('/add_task', function (req, res) {
+    const userText = req.body.task
+    console.log('User entered:', userText)
+    
+    res.render('user', { name: userText })  // <-- pass data to user.pug
+})
 
-// Start the web server
+// Start server
 app.listen(3000, function () {
-  console.log('Listening on port 3000...')
+    console.log('Listening on port 3000...')
 })
+
