da17e30fb32a2ee679ec8514f223dbaf5ad6b61c
Brayden Xenos <braydenxenos@u.boisestate.edu>
2025-04-25 19:54:32 -0600

    created initial files

---
+++ User.java
@@ -0,0 +1,15 @@
+public class User {
+    //instance variables
+    String name;
+    String school;
+    
+    /**
+     * constructor to make user
+     */
+    public User(String name, String school) {
+        this.name = name;
+        this.school = school;
+
+        
+    }
+}
