1ab4d1829d44d165dd1c8945fa727f2e9ea92741
Will White <willwhite687@u.boisestate.edu>
2025-04-25 20:05:47 -0600

    Started Schedule.java

--- Schedule.java
+++ Schedule.java
@@ -1,8 +1,41 @@
-import java.time.*;;
+import java.time.*;
+import java.util.ArrayList;;
 
+/**
+ * A Schedule is an ArrayList of Course objects. A Schedule object is used to manage and store courses input by the user.
+ * 
+ * @author Will White, Brayden Xenos
+ */
 public class Schedule {
-    //array list of courses
-    public Schedule(LocalTime starTime, LocalTime endTime, String courseName) {
-        //DO SOMETHING
+
+    private ArrayList<Course> schedule;
+
+    /**
+     * Constructs an empty Schedule with no Courses
+     */
+    public Schedule() {
+        this.schedule = new ArrayList<Course>();
     }
+
+    /**
+     * Constructs a Schedule with an input Course
+     */
+    public Schedule(Course course) {
+        this.schedule = new ArrayList<Course>();
+        this.schedule.add(course);
+    }
+
+    /**
+     * Adds a Course to the ArrayList
+     */
+    public void addCourse(Course course) {
+        this.schedule.add(course);
+    } 
+
+    /**
+     * Removes a Course to the ArrayList
+     */
+    public void removeCourse(Course course) {
+        this.schedule.remove(course);
+    } 
 }
